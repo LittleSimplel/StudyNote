437b05f2c74a22178afcfbf5627f9dec4012cec9
zyc <1046202517@qq.com>
2021-08-18 19:36:45 +0800

    1

---
+++ 开发/jenkins/deploy.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+#接收外部参数
+harbor_url=$1
+harbor_project_name=$2
+project_name=$3
+tag=$4
+port=$5
+imageName=$harbor_url/$harbor_project_name/$project_name:$tag
+echo "$imageName" 
+#查询容器是否存在，存在则删除
+containerId=`docker ps -a | grep -w ${project_name}:${tag} | awk '{print $1}'`
+if [ "$containerId" != "" ] ; then
+#停掉容器
+docker stop $containerId
+#删除容器
+docker rm $containerId
+echo "成功删除容器"
+fi
+#查询镜像是否存在，存在则删除
+imageId=`docker images | grep -w $project_name | awk '{print $3}'`
+if [ "$imageId" != "" ] ; then
+#删除镜像
+docker rmi -f $imageId
+echo "成功删除镜像"
+fi
+# 登录Harbor私服
+docker login -u admin -p Harbor12345 $harbor_url
+# 下载镜像
+docker pull $imageName
+# 启动容器
+# docker run -di -p $port:$port $imageName
+docker run --restart=always -d -v /root/logs:/root/logs -p $port:$port $imageName
+echo "容器启动成功"
